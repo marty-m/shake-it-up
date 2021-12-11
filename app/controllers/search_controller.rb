e61f8cc11b9415167e8e9d5797e9a0a8ce7c6da2
marty-m <martynas.miliauskas0@gmail.com>
2021-12-11 16:22:48 +0100

    imported front-end by @Lakshita110, added a few JS scripts, hosted background images on AWS S3

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,12 @@
 class SearchController < ApplicationController
   def searchResults
-    @response = HTTParty.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{SEARCHVAR}")
+    @response = HTTParty.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{@searchText}")
     @jdoc = JSON.parse(@response.body)
     @jdoc["drinks"].each  do |sub|
       puts sub["strDrink"]
+    end
   end
 
   def cocktailPage
-
   end
-  end
-  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'search/searchResults'
   get 'search/cocktailPage'
   root 'home#index'
+  post 'search_form', to:'search#searchResults', as: 'searchResults'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
