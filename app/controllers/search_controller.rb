c555b86e1aa30353c10a6bd81a640d4454607cae
marty-m <martynas.miliauskas0@gmail.com>
2021-12-11 23:51:53 +0100

    attempt to pass ID of clicked cocktail through a file but realized it isnt possible in MVC system. Built out a functional final page if a cocktail is found by ID. onyl passing of id and fixing front-end is left.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,15 @@
 class SearchController < ApplicationController
 
-
-
   def new
     @search = Search.new
   end
 
   def searchResults
+
     @searchText = params[:searchInput]
     @nameArr = []
     @urlArr =[]
+    @IdArr = []
     @hash1 = {}
     @response = HTTParty.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{@searchText}")
     @jdoc1 = JSON.parse(@response.body)
@@ -19,27 +19,35 @@ class SearchController < ApplicationController
     @jdoc1["drinks"].each  do |sub|
       @urlArr.push("#{sub["strDrinkThumb"]}/preview")
     end
+    @jdoc1["drinks"].each  do |sub|
+      @IdArr.push(sub["idDrink"])
+      puts @IdArr
+    end
       for n in (0..@nameArr.size-1) do
         @hash1[@nameArr[n]] = @urlArr[n]
       end
   end
 
   def cocktailPage
-
-
-    @name =  sub["strDrink"]
-    if sub["strCategory"] != nil then @category = sub["strCategory"] end
-    @alco = sub["strAlcoholic"]
-    @glass = sub["strGlass"]
-    @inst = sub["strInstructions"]
-    @photoUrl = "#{sub["strDrinkThumb"]}/preview"
+    @selectedId = File.read('\Users\noodle-box\IdeaProjects\ShakeItUp\app\controllers\ID.txt')
+    puts "ID HERE: #{params[:id].nil?}"
+    @hash2 = {}
+    @response = HTTParty.get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{params[:id]}")
+    @jdoc3 = JSON.parse(@response.body)
+    @jdoc3["drinks"].each  do |sub|
+    @name2 =  sub["strDrink"]
+    @photoUrl2 = "#{sub["strDrinkThumb"]}/preview"
+    if sub["strCategory"] != nil then @category2 = sub["strCategory"] end
+    @alco2 = sub["strAlcoholic"]
+    @glass2 = sub["strGlass"]
+    @inst2 = sub["strInstructions"]
+    @photoUrl2 = "#{sub["strDrinkThumb"]}/preview"
     for i in (1..15) do
       if (sub["strIngredient#{i}"] != nil) && (sub["strIngredient#{i}"] != " ")
-        @hash[sub["strIngredient#{i}"]] = sub["strMeasure#{i}"]
+        @hash2[sub["strIngredient#{i}"]] = sub["strMeasure#{i}"]
       end
     end
-
-
+    end
   end
 
   def randomPage
@@ -48,7 +56,6 @@ class SearchController < ApplicationController
     @jdoc = JSON.parse(@responseRand.body)
     @jdoc["drinks"].each  do |sub|
       @name =  sub["strDrink"]
-      puts @name
       if sub["strCategory"] != nil then @category = sub["strCategory"] end
       @alco = sub["strAlcoholic"]
       @glass = sub["strGlass"]
